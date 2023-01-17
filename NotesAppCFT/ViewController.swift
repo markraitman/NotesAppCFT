bc9b9534911185b55df78d8aee8b1ec60be0465b
markraitman <markraitman@mail.ru>
2023-01-17 14:33:37 +0300

    Initial Commit

---
+++ NotesAppCFT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesAppCFT
+//
+//  Created by Марк Райтман on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
